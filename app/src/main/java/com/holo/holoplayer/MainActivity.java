fafeb2aabeeec1966afc703cf42ecba42794c7d1
wangchengge <wangchengge@do-global.com>
2020-09-22 12:01:18 +0800

    新增viewBinding,新建dev分支

--- app/src/main/java/com/holo/holoplayer/MainActivity.java
+++ app/src/main/java/com/holo/holoplayer/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.holo.holoplayer.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding mMainBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mMainBinding.getRoot());
+        
     }
 }
