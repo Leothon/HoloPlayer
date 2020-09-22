5bba76b8d682a367ad2089d8a2e53ec99a4f037b
wangchengge <wangchengge@do-global.com>
2020-09-22 16:21:37 +0800

    播放器抽象

--- app/src/main/java/com/holo/holoplayer/MainActivity.java
+++ app/src/main/java/com/holo/holoplayer/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         mMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mMainBinding.getRoot());
-        
+
     }
 }
---
+++ holo/src/main/java/com/holo/holo/base/PlayerManager.java
@@ -0,0 +1,10 @@
+package com.holo.holo.base;
+
+/**
+ * @Author: a10943
+ * @Date: 2020/9/22
+ * @Desc: 播放器管理类，管理配置等
+ */
+public class PlayerManager {
+
+}
